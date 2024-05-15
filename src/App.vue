4e94b365273533f72eaf90af4b9c98c24b943492
Dzmitry Baryshau <dima.baryshev@live.com>
2024-05-15 22:50:52 +0300

    feat: add Header, Card components, icons

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div class="w-4/5 m-auto bg-white min-h-dvh rounded-xl shadow-2xl mt-14">
+    <Header />
+    <main class="px-10 py-5">
+      <section>
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="grid grid-cols-3 gap-5">
+          <Card title="Sneakers nike" src="/sneakers/sneakers-1.jpg" price="1200" />
+          <Card title="Sneakers nike" src="/sneakers/sneakers-1.jpg" price="1200" />
+          <Card title="Sneakers nike" src="/sneakers/sneakers-1.jpg" price="1200" />
+          <Card title="Sneakers nike" src="/sneakers/sneakers-1.jpg" price="1200" />
+          <Card title="Sneakers nike" src="/sneakers/sneakers-1.jpg" price="1200" />
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="flex justify-between border-b-2 border-slate-300 px-10 py-8">
+    <div class="flex gap-4 items-center">
+      <img class="w-10 h-10" src="/logo.png" alt="logo" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <span class="text-slate-400"> Магазин лучших кроссовок</span>
+      </div>
+    </div>
+    <ul class="flex items-center justify-between gap-10">
+      <li>
+        <a class="flex gap-2 text-gray-500 hover:text-black cursor-pointer"
+          ><img src="/cart.svg" alt="cart" /> <b>1205 руб.</b></a
+        >
+      </li>
+      <li>
+        <a class="flex gap-2 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="favorite" /> <span> Закладки</span></a
+        >
+      </li>
+      <li>
+        <a class="flex gap-2 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/profile.svg" alt="profile" /> <span> Профиль</span></a
+        >
+      </li>
+    </ul>
+  </header>
+</template>
